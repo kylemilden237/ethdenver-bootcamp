f5d438b72ee2b26c75237ce7b845826a11356682
kylemilden237 <84802200+kylemilden237@users.noreply.github.com>
2022-10-20 12:04:22 -0400

    Update Bootcamp.sol

--- hw3/Bootcamp.sol
+++ hw3/Bootcamp.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.17;
 contract BootcampContract {
 
     uint256 number;
-    address public deployer;
+    address deployer;
 
     constructor() {
         deployer = msg.sender;
